41eb347b68ce85093d5ddd2331acc70118dbe47f
M4RT1N <mjlynch35@yahoo.com>
2023-04-05 22:57:21 -0500

    Formatted code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const fs = require("fs");
-const uuid = require('./Helpers/uuid');
+const uuid = require("./Helpers/uuid");
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,30 +10,30 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // * Routing to the home page
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // * Sending the user to the notes page
 app.get("/notes", (req, res) => {
-    res.sendFile(__dirname + "/public/notes.html");
-})
+  res.sendFile(__dirname + "/public/notes.html");
+});
 
 // * Reading the data from the backend and transfering it to the frontend
 app.get("/api/notes", (req, res) => {
-    // * Read data from JSON file
+  // * Read data from JSON file
   const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8")) || [];
 
   // * Transfering that data to the frontend
   res.json(data);
-})
+});
 
 // * Posting the notes that we have added
 app.post("/api/notes", (req, res) => {
-    // * We are reading the data from the file and assigning it to data, if data is empty we will assign it to an empty array
+  // * We are reading the data from the file and assigning it to data, if data is empty we will assign it to an empty array
   const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8")) || [];
 
   // * Getting items from request body
@@ -48,27 +48,26 @@ app.post("/api/notes", (req, res) => {
 
   // * Sending a success message
   res.json({ success: true });
-
-})
+});
 
 // * This is the code for deleting an item
 app.delete("/api/notes/:id", (req, res) => {
-    const data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+  const data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
   const noteId = req.params.id;
 
   // * Finding the note with a matching ID
-  const noteIndex = data.findIndex(note => note.id === noteId);
+  const noteIndex = data.findIndex((note) => note.id === noteId);
 
   // * If that note is in the array then we will remove it from the array
   if (noteIndex !== -1) {
     data.splice(noteIndex, 1);
     // * Rewriting data to the file minus the note with the matching ID
-    fs.writeFileSync('./db/db.json', JSON.stringify(data, null, 2));
+    fs.writeFileSync("./db/db.json", JSON.stringify(data, null, 2));
     // * Printing success message
     res.json({ success: true });
   } else {
     // * Sending a failure message
-    res.status(404).json({ error: 'Note not found' });
+    res.status(404).json({ error: "Note not found" });
   }
 });
 
